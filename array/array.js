d053fb57ba27441c6be896afd776e964ed47c08c
Claudio Vazquez <claudio.vazquez@konfio.mx>
2022-08-11 23:49:00 -0500

    1: Arrays

---
+++ array/array.js
@@ -0,0 +1,46 @@
+class MyArray {
+    constructor() {
+        this.length = 0
+        this.data = {}
+    }
+
+    get(index){
+        return this.data[index]
+    }
+
+    push(item) {
+        this.data[this.length] = item
+        this.length++
+        return this.data
+    }
+
+    pop() {
+        const lastIndex = this.length -1
+        const lastItem = this.data[lastIndex]
+        delete this.data[lastItem]
+        this.length--
+        return lastItem
+    }
+
+    delete(index){
+        const item = this.data[index]
+        this.shiftIndex(index)
+        return item
+    }
+
+    shiftIndex(index) {
+        for (let i = index; i < this.length -1; i++) {
+            this.data[i] = this.data[i+1]
+        }
+        delete this.data[this.length-1]
+        this.length--
+    }
+
+}
+
+let fruits = new MyArray()
+console.log(fruits.push("Pera"))
+console.log(fruits.push("platano"))
+const lastItemTem = fruits.delete(0)
+console.log(lastItemTem)
+console.log(fruits)
